cbdf991e8e5a0704950a9800d8e0329ddc0e158a
W301-07\student <gundam@gmail.com>
2017-05-25 18:26:34 +0900

    Add Hitjudgment

---
+++ GameSource/Hitjudgment.h
@@ -0,0 +1,13 @@
+#pragma once
+#include"../Source/System/GameObject.h"
+class Hitjudgment
+{
+public:
+	static bool Colliding(Elixir::GameObject* obj1,Elixir::GameObject* obj2);
+
+	
+
+private:
+
+};
+
