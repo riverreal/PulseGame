d74bf6ed15996b1db9d55cc6a2bab99f08629284
YuFukai201 <yu.fukai0201@gmail.com>
2017-05-25 18:29:40 +0900

    Add AIdiff

--- GameSource/AI.cpp
+++ GameSource/AI.cpp
@@ -1,6 +1,7 @@
 #include"AI.h"
-
-
+#include"../Source/System/Audio/AudioManager.h"
+#include"../Source/Helper/ENote.h"
+using namespace Elixir;
 AI::~AI()
 {
 }
@@ -11,10 +12,107 @@ void AI::Initialize(Elixir::SceneManager* sceneManager, int playerNum, PlayerShi
 
 	m_NotesTiming = rhythm->GetNotesTiming();
 	m_NotesLaneNumber = rhythm->GetNotesLaneNumber();
+	
+	m_rhythm = rhythm;
+	m_ship = pship;
+
+	m_randNum = rand() % 100;
+	
+	//m_diff = ENote::GetInstance().Notify<int>("AiDiff");
+	m_diff = 1;
+
 }
 
 void AI::Update(float dt)
 {
+	
+	CommandInterpreter::GetInstance().setRightNotes(m_playerNum, false);
+	CommandInterpreter::GetInstance().setCenterNotes(m_playerNum, false);
+	CommandInterpreter::GetInstance().setLeftNotes(m_playerNum, false);
 
+	if(!m_musicFinish)
+		RhythmHitCheck();
+
+	Log() << m_randNum;
+	
 }
 
+void AI::RhythmHitCheck()
+{
+	if (m_randNum > m_HitDiff[m_diff][0])
+	{
+		if (m_NotesTiming[m_TimingCount] - (int)AudioManager::GetInstance().GetControlledMusic()->getPlayPosition() <= m_rhythm->GREAT_TIME / 2)
+		{
+			if (m_NotesLaneNumber[m_TimingCount] == 0)
+				CommandInterpreter::GetInstance().setRightNotes(m_playerNum, true);
+			else if (m_NotesLaneNumber[m_TimingCount] == 1)
+				CommandInterpreter::GetInstance().setCenterNotes(m_playerNum, true);
+			else if (m_NotesLaneNumber[m_TimingCount] == 2)
+				CommandInterpreter::GetInstance().setLeftNotes(m_playerNum, true);
+
+			if (m_TimingCount != m_NotesLaneNumber.size() - 1)
+				m_TimingCount++;
+			else
+				m_musicFinish = true;
+			
+			m_randNum = rand() % 100;
+		}
+	}
+	else if (m_randNum > m_HitDiff[m_diff][1])
+	{
+		if (m_NotesTiming[m_TimingCount] - (int)AudioManager::GetInstance().GetControlledMusic()->getPlayPosition() <= m_rhythm->GOOD_TIME / 2)
+		{
+			if (m_NotesLaneNumber[m_TimingCount] == 0)
+				CommandInterpreter::GetInstance().setRightNotes(m_playerNum, true);
+			else if (m_NotesLaneNumber[m_TimingCount] == 1)
+				CommandInterpreter::GetInstance().setCenterNotes(m_playerNum, true);
+			else if (m_NotesLaneNumber[m_TimingCount] == 2)
+				CommandInterpreter::GetInstance().setLeftNotes(m_playerNum, true);
+
+			if (m_TimingCount != m_NotesLaneNumber.size() - 1)
+				m_TimingCount++;
+			else
+				m_musicFinish = true;
+			
+			m_randNum = rand() % 100;
+		}
+	}
+	else if (m_randNum > m_HitDiff[m_diff][2])
+	{
+		if (m_NotesTiming[m_TimingCount] - (int)AudioManager::GetInstance().GetControlledMusic()->getPlayPosition() <= m_rhythm->BAD_TIME / 2)
+		{
+			if (m_NotesLaneNumber[m_TimingCount] == 0)
+				CommandInterpreter::GetInstance().setRightNotes(m_playerNum, true);
+			else if (m_NotesLaneNumber[m_TimingCount] == 1)
+				CommandInterpreter::GetInstance().setCenterNotes(m_playerNum, true);
+			else if (m_NotesLaneNumber[m_TimingCount] == 2)
+				CommandInterpreter::GetInstance().setLeftNotes(m_playerNum, true);
+
+			if (m_TimingCount != m_NotesLaneNumber.size() - 1)
+				m_TimingCount++;
+			else
+				m_musicFinish = true;
+			
+			m_randNum = rand() % 100;
+		}
+	}
+	else
+	{
+		if (m_NotesTiming[m_TimingCount] - (int)AudioManager::GetInstance().GetControlledMusic()->getPlayPosition() <= m_rhythm->BAD_TIME*0.5f + 20)
+		{
+			if (m_NotesLaneNumber[m_TimingCount] == 0)
+				CommandInterpreter::GetInstance().setRightNotes(m_playerNum, true);
+			else if (m_NotesLaneNumber[m_TimingCount] == 1)
+				CommandInterpreter::GetInstance().setCenterNotes(m_playerNum, true);
+			else if (m_NotesLaneNumber[m_TimingCount] == 2)
+				CommandInterpreter::GetInstance().setLeftNotes(m_playerNum, true);
+
+			if (m_TimingCount != m_NotesLaneNumber.size() - 1)
+				m_TimingCount++;
+			else
+				m_musicFinish = true;
+			
+			m_randNum = rand() % 100;
+		}
+	}
+}
--- GameSource/AI.h
+++ GameSource/AI.h
@@ -2,6 +2,7 @@
 
 #include"RhythmManager.h"
 #include "PlayerShip.h"
+#include "CommandInterpreter.h"
 
 
 class AI
@@ -14,11 +15,35 @@ public:
 
 private:
 
+	enum HitNumber 
+	{
+		HN_GREAT = 8,
+		HN_GOOD = 3,
+		HN_BAD = 1
+	};
+
+	int m_HitDiff[3][3]
+	{
+		///{GREAT,GOOD,BAD}
+		{85,60,20},	//Easy
+		{75,40,10 },//Normal
+		{65,20,5}//Hard
+	};
+
+
 	int m_playerNum;
-	int timingCount = 0;
+	int m_TimingCount = 0;
+	int m_randNum;
+	int m_diff;
+	bool m_musicFinish = false;
+
+	void RhythmHitCheck();
 
 	std::vector<int>m_NotesLaneNumber;
 	std::vector<double>m_NotesTiming;
+
+	RhythmManager* m_rhythm;
+	PlayerShip* m_ship;
 
 };
 
